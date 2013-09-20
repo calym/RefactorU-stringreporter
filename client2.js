abb568e0a842b09c33890fec38c3b421595a5b28
RU-019 <RU-019@Admins-Mac-mini-61.local>
2013-09-20 12:17:53 -0600

    first commit

---
+++ client2.js
@@ -0,0 +1,11 @@
+
+
+var anyword = prompt("Enter any word.")
+alert("You entered the word " + anyword + ".\n \
+	It has " + anyword.length + " characters. \n \
+	The 3rd character is " + anyword.charAt(2) + ". \n \
+	Lowercase: " + anyword.toLowerCase() + ". \n \
+	Uppercase: " + anyword.toUpperCase() + ". \n \
+	The dog jumped over the " + anyword + ". \n \
+	Subword: " + anyword.substring(2,4)) 
+	
